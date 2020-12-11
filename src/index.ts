e3a8b1bcbf30bd7d54cb8cc739709b534eaa697f
Francois Best <francois@francoisbest.com>
2020-12-11 09:06:18 +0100

    fix: Remove automatic inclusion of Image
    Doing so adds `next/image` in the imports tree,
    which is enough to automatically opt-in to Next.js
    Image Optimization feature, which has drawbacks:
    - Breaks static export
    - Requires a server / serverless setup
    - Requires setting up the Image Optimization config
    
    Buying in the Image component should be explicit,
    so imports will be separated.
    
    See #187.

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,15 @@ export * from './components/links'
 export * from './components/redirect'
 export * from './components/svg'
 export * from './components/no-ssr'
-export * from './components/image'
 export * from './pages/_app'
 export * from './ui/theme'
 export * from './ui/colors'
 export * from './ui/fonts'
+
+// Note: Image is not exported, as doing so automatically opts-in
+// the Next.js Image Optimization feature, which requires a server
+// or an API to work, and breaks static exports, even if no Image
+// component is used.
+//
+// To use the preview Chakra+Next Image component, import it as:
+// import { Image } from '@47ng/chakra-next/dist/components/image
